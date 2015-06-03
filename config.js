a035b9b8176de459b5a8a536fcdd9f40afd2a019
Gonzalo Pozzo <goncy@live.com.ar>
2015-06-02 22:11:44 -0300

    Commit
    Comi

--- config.js
+++ config.js
@@ -1,12 +1,21 @@
 var chatter = require('chatter');
 
-module.exports = function(app, express){
-	// Set .ejs as the default template extension
-	app.set('view engine', 'ejs');
+module.exports = function(app, express) {
+    // Set .ejs as the default template extension
+    app.set('view engine', 'ejs');
 
-	// Tell express where it can find the templates
-	app.set('views', __dirname + '/views');
+    // Tell express where it can find the templates
+    app.set('views', __dirname + '/views');
 
-	//Declaracion de la carpeta public
-	app.use('/public', express.static('public'));
+    //Declaracion de la carpeta public
+    app.use('*/public', express.static('public'));
+
+    // handling 404 errors
+	app.use(function(err, req, res, next) {
+	  if(err.status !== 404) {
+	    return next();
+	  }
+	 
+	  res.send(err.message || '** no unicorns here **');
+	});
 }
--- routes.js
+++ routes.js
@@ -1,31 +1,69 @@
 var chatter = require('chatter');
 var sala = "";
+var error = new Error();
 
-module.exports = function(app,io){
-	//Rutas para la redireccion de la sala
-	app.get('/:sala', function(req, res, next){
-		sala = req.params.sala;
-		var password = io.of(sala).password;
-		if(sala.length > 20) res.render('error', {error: "Sala con nombre muy largo"});
-		else if(password && req.query.password != password) res.render('error', {error: "Pass incorrecta"});
-		else if(password && req.query.password == password)	res.render('index', {title:sala});
-		else if(sala.length < 20 && !password) res.render('index', {title:sala});
-	});
+module.exports = function(app, io) {
+    //Seteo de redireccion de sockets
+    chatter.set_sockets(io.sockets);
 
-	//Handling error de rutas
-	app.get('/*', function(req, res, next){
-	 	res.redirect("/principal");
-	});
+    //Redireccion de sockets en conexion
+    io.sockets.on('connection', function(socket) {
+        chatter.connect_chatter({
+            socket: socket,
+            sala: sala,
+            io: io
+        });
+    });
 
-	//Seteo de redireccion de sockets
-	chatter.set_sockets(io.sockets);
+    function getPassword(sala_id) {
+        return io.of(sala_id).password;
+    }
 
-	//Redireccion de sockets en conexion
-	io.sockets.on('connection', function(socket){
-		chatter.connect_chatter({
-			socket: socket,
-			sala: sala,
-			io: io
-		});
-	});
+    function returnError(res, error) {
+        res.render('error', {
+            error: error
+        });
+    }
+
+    var setParams = function(req, res, next) {
+        req.sala = req.params.sala ? req.params.sala : "principal";
+        req.password_sala = getPassword(req.sala);
+        req.password = req.params.password;
+        error.message = "";
+        return next();
+    }
+
+    var checkErrors = function(req, res, next) {
+        if (req.password_sala && !req.password) error.message = "Esta sala tiene password";
+        else if (req.password && !req.password_sala) error.message = "Esta sala no tiene password";
+        else if (req.password != req.password_sala) error.message = "La contraseña es incorrecta";
+        else if (req.sala.length > 20) error.message = "Sala con nombre muy largo";
+        return next();
+    }
+
+    var routeHandler = function(req, res) {
+        sala = req.params.sala;
+        if (error.message) res.render('error', {
+            error: error.message
+        });
+        else res.render('index', {
+            title: req.sala
+        });
+    }
+
+    var checkers = [setParams, checkErrors, routeHandler];
+    //Rutas para la redireccion de la sala
+    app.get(['/', '/:sala', '/:sala/:password'], checkers);
+
+    app.get('*', function(req, res, next) {
+        error.message = "No se encontro la pagina";
+        next(error);
+    });
+
+    // handling 404 errors
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            error: err.message || 'Hubo un error, fijate que todo sea correcto'
+        });
+    });
 }
